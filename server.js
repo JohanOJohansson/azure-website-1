de6e5613318380fb7dca8ecfc194b72a0ee87e49
Johansson, Johan <johan.johansson@elekta.com>
2015-04-21 14:46:43 +0200

    Disabled the logging command

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
-    console.log('serving a request');
+    //console.log('serving a request');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port);
