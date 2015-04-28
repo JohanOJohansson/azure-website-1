f2b79170a732894a319cad6e6da7e56fa07dba1d
Johansson, Johan <johan.johansson@elekta.com>
2015-04-28 18:48:11 +0200

    Added Winston logging and good in order to log stuff (I think)

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
-var Hapi = require('hapi')
+var Hapi = require('hapi');
+var GoodWinston = require('good-winston');
+var winston = require('winston');
 //var http = require('http')
 
 /*
@@ -15,6 +17,24 @@ http.createServer(function(req, res) {
 */
 
 var server = new Hapi.Server();
+server.register({
+	register: require('good'), 
+	options: {
+		reporters: [
+			new GoodWinston({
+				ops: '*', 
+				request: '*', 
+				response: '*', 
+				log: '*', 
+				error: '*'
+			}, winston)
+		]
+	}
+}, function(err) {
+	if (err) {
+		return server.log(['error'], 'good load error: ' + err);
+	}
+});
 
 if ( process.env.PORT ) {
 	server.connection({ port: process.env.PORT });
